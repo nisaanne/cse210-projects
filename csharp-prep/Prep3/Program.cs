40c807d1d50d6513ad045138860bc33fddbe8b21
nisaanne <youremail@nisaanne@byui.edu>
2024-10-29 12:49:20 -0700

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+
+        int myguess = -1;
+
+        while (myguess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            myguess = int.Parse(guess);
+        
+        
+            if (myguess < magicNumber)
+                Console.WriteLine("Higher");
+
+            else if (myguess > magicNumber)
+                Console.WriteLine("Lower");
+
+            else 
+            Console.WriteLine("You Guessed it!");
+        }
+       
     }
 }
