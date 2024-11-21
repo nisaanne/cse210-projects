808f9848f4903ef8dc9b8287803d545b6d597de6
nisaanne <youremail@nisaanne@byui.edu>
2024-11-21 09:57:57 -0800

    naming conventions

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -6,23 +6,23 @@ public class Program
     public static void Main()
     {
        
-        var video1 = new Video("Glass marble tutorial", "Venisa McAllister", 600);
+        Video video1 = new Video("Glass marble tutorial", "Venisa McAllister", 600);
         video1.AddComment(new Comment("Alice", "Great tutorial!"));
         video1.AddComment(new Comment("Bob", "Very helpful, thanks!"));
         video1.AddComment(new Comment("Charlie", "Well explained."));
 
-        var video2 = new Video("Dirtbike Maintainance", "Samuel McAllister", 900);
+        Video video2 = new Video("Dirtbike Maintainance", "Samuel McAllister", 900);
         video2.AddComment(new Comment("Dave", "Awesome content!"));
         video2.AddComment(new Comment("Eve", "Really enjoyed this."));
         video2.AddComment(new Comment("Frank", "Looking forward to more videos."));
 
-        var video3 = new Video("Nativity Play", "Emily McAllister", 750);
+        Video video3 = new("Nativity Play", "Emily McAllister", 750);
         video3.AddComment(new Comment("Grace", "This was amazing!"));
         video3.AddComment(new Comment("Heidi", "Thanks for sharing."));
         video3.AddComment(new Comment("Ivan", "Can't wait for the next one."));
 
         
-        var videos = new List<Video> { video1, video2, video3 };
+        List<Video> videos = new List<Video> { video1, video2, video3 };
 
         
         foreach (var video in videos)
--- foundation/Foundation1/video.cs
+++ foundation/Foundation1/video.cs
@@ -6,26 +6,26 @@ public class Video
     public string Title { get; set; }
     public string Author { get; set; }
     public int Length { get; set; }
-    private List<Comment> Comments { get; set; }
+    private List<Comment> _comments { get; set; }
 
     public Video(string title, string author, int length)
     {
         Title = title;
         Author = author;
         Length = length;
-        Comments = new List<Comment>();
+        _comments = new List<Comment>();
     }
 
     
     public void AddComment(Comment comment)
     {
-        Comments.Add(comment);
+        _comments.Add(comment);
     }
 
    
     public int GetNumberOfComments()
     {
-        return Comments.Count;
+        return _comments.Count;
     }
 
      
@@ -36,7 +36,7 @@ public class Video
         Console.WriteLine($"Length: {Length} seconds");
         Console.WriteLine($"Number of Comments: {GetNumberOfComments()}");
         Console.WriteLine("Comments:");
-        foreach (var comment in Comments)
+        foreach (var comment in _comments)
         {
             Console.WriteLine($"- {comment.Name}: {comment.Text}");
         }
