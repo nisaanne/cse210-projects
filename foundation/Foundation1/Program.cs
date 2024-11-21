f10ba4ada53ba6719fd7e5f51849dd5030ea78bd
nisaanne <youremail@nisaanne@byui.edu>
2024-11-21 09:45:32 -0800

    fixed files

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -1,63 +1,6 @@
 using System;
 using System.Collections.Generic;
 
-
-public class Comment
-{
-    public string Name { get; set; }
-    public string Text { get; set; }
-
-    public Comment(string name, string text)
-    {
-        Name = name;
-        Text = text;
-    }
-}
-
-
-public class Video
-{
-    public string Title { get; set; }
-    public string Author { get; set; }
-    public int Length { get; set; }
-    private List<Comment> Comments { get; set; }
-
-    public Video(string title, string author, int length)
-    {
-        Title = title;
-        Author = author;
-        Length = length;
-        Comments = new List<Comment>();
-    }
-
-    
-    public void AddComment(Comment comment)
-    {
-        Comments.Add(comment);
-    }
-
-    
-    public int GetNumberOfComments()
-    {
-        return Comments.Count;
-    }
-
-    
-    public void DisplayVideoInfo()
-    {
-        Console.WriteLine($"Title: {Title}");
-        Console.WriteLine($"Author: {Author}");
-        Console.WriteLine($"Length: {Length} seconds");
-        Console.WriteLine($"Number of Comments: {GetNumberOfComments()}");
-        Console.WriteLine("Comments:");
-        foreach (var comment in Comments)
-        {
-            Console.WriteLine($"- {comment.Name}: {comment.Text}");
-        }
-    }
-}
-
-
 public class Program
 {
     public static void Main()
