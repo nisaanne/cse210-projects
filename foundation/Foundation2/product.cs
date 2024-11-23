c6494e1f28bdab68515418f6fbfd7d755fee4e86
nisaanne <youremail@nisaanne@byui.edu>
2024-11-23 09:06:25 -0800

    correct product error

--- foundation/Foundation2/product.cs
+++ foundation/Foundation2/product.cs
@@ -5,24 +5,24 @@ public class Product
     private double price;
     private int quantity;
 
-
-    public Product(string name, string productID, double price, int quantity)
+    
+    public Product(string name, string productId, double price, int quantity)
     {
         this.name = name;
-        this.productId = productId;
+        this.productId = productId; 
         this.price = price;
-        this.quantity = quantity;
-
+        
     }
 
+    
     public double GetTotalCost()
     {
-        return this.price * this.quantity;
+        return this.price;
     }
 
+    
     public override string ToString()
     {
-        return $"{this.name} (ID: {this.productID})";
+        return $"{this.name} (ID: {this.productId})";
     }
-
 }
