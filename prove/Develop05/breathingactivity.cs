c28ca603c3d924ee91a06b0f8ceab2d362a31e8f
nisaanne <youremail@nisaanne@byui.edu>
2024-11-26 08:12:38 -0800

    update breathing

--- prove/Develop05/breathingactivity.cs
+++ prove/Develop05/breathingactivity.cs
@@ -10,43 +10,21 @@ public class BreathingActivity : MindfulnessActivity
         int duration = GetDuration();
         for (int i = 0; i < duration; i += 10)
         {
-            Console.WriteLine("Prepare to Breathe in...");
+            Console.Write("Breathe in...");
             Countdown(5);
-            Console.WriteLine("Breathe in...");
-            SlowBreathingAnimation(5);
-
-            Console.WriteLine("Prepare to Breathe out...");
+            Console.Write("Breathe out...");
             Countdown(5);
-            Console.WriteLine("Breathe out...");
-            SlowBreathingAnimation(5);
         }
-    }
-
-    private void SlowBreathingAnimation(int seconds)
-    {
-        DateTime startTime = DateTime.Now;
-        DateTime futureTime = startTime.AddSeconds(seconds);
-
-        while (DateTime.Now < futureTime)
-        {
-            Console.Write("+");
-            Thread.Sleep(1000); 
-            Console.Write("\b \b"); 
-            Console.Write("-");
-            Thread.Sleep(1000); 
-            Console.Write("\b \b"); 
-        }
-        Console.WriteLine();
     }
 
     private void Countdown(int seconds)
     {
         for (int i = seconds; i > 0; i--)
         {
-            Console.WriteLine(i);
-            Thread.Sleep(1000); 
+            Console.Write($" {i}");
+            Thread.Sleep(1000); // One second pause between countdown numbers
+            Console.Write("\b\b"); // Erase the previous number
         }
+        Console.WriteLine(" 0");
     }
 }
-
-
